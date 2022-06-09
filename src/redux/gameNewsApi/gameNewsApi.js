80703b93b08d8a892c500855dbf64bead0cfa68a
Neo <neofranco9@gmail.com>
2022-06-09 10:12:30 +0800

    fix news api redirects

--- src/redux/gameNewsApi/gameNewsApi.js
+++ src/redux/gameNewsApi/gameNewsApi.js
@@ -3,10 +3,9 @@ import axios from "axios";
 
 const fetchNews = async () => {
   const apiKey = process.env.REACT_APP_NEWSAPI_APIKEY
-  const topic = 'video+games'
 
   const response = await axios({
-    url: `/news-api/${topic}`,
+    url: '/news-api',
     method: 'GET',
     headers: {
       'Authorization': apiKey
